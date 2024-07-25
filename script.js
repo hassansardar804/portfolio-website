630255a1d176f8068a61523adae9fa0946ff9d3a
hassansardar804 <hassan.sardar.cyber@gmail.com>
2024-07-25 07:11:48 -0700

    created header of portfolio website

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i>Web </i> Developer','<i>Mern </> Stack Developer', '&amp; Cyber Expert...'],
+    typeSpeed: 60,
+    loop: true,
+    backSpeed: 40, 
+    backDelay: 1000, 
+});
